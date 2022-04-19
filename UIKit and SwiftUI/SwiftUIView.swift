affa6423dac26a4e5f0a7a346a831b03d5081a61
iremkaraoglu <iremk97@gmail.com>
2022-04-19 14:39:04 +0300

    create SwiftUI Screen

---
+++ UIKit and SwiftUI/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  UIKit and SwiftUI
+//
+//  Created by Irem Karaoglu on 19.04.2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("SwiftUI Screen")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
