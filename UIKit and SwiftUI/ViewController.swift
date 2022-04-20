6fe989ecbeeeb8f72f293dc983d8d8f4a9b73c0f
iremkaraoglu <iremk97@gmail.com>
2022-04-20 13:56:37 +0300

    create UIKit screen

--- UIKit and SwiftUI/ViewController.swift
+++ UIKit and SwiftUI/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.text = "UIKit Screen"
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        let button = UIButton()
+        button.setTitleColor(.blue, for: .normal)
+        button.layoutMargins = .init(top: 48, left: 0, bottom: 0, right: 0)
+        button.setTitle("Navigate to SwiftUI Screen", for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let container = UIView()
+        container.addSubview(label)
+        container.addSubview(button)
+        container.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(container)
+        
+        NSLayoutConstraint.activate([
+            container.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            container.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            container.widthAnchor.constraint(equalTo: view.widthAnchor),
+            container.heightAnchor.constraint(equalToConstant: 110),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 64)])
     }
-
-
 }
 
