2217b4783a5ebfa15a7a23ab3d632e0e84b09142
iremkaraoglu <iremk97@gmail.com>
2022-04-21 13:42:02 +0300

    add navigation from SwiftUI to UIKit screen

--- UIKit and SwiftUI/SwiftUIView.swift
+++ UIKit and SwiftUI/SwiftUIView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct SwiftUIView: View {
+    weak var navigationController: UINavigationController?
     var body: some View {
-        Text("SwiftUI Screen")
-    }
+           VStack {
+               Text("SwiftUI Screen")
+               Button(action: {
+                   navigationController?.popViewController(animated: true)
+               }) {
+                   Text("Navigate to UIKit Screen")
+               }.padding(48)
+           }.navigationBarHidden(true)
+       }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
